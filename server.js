9acebd9a94b95117f2ff408fcd73d9b2f63c773a
Dhanasekar <sendmailtodhana@gmail.com>
2020-08-03 21:02:08 +0530

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var config = require('./conf.json');
+var port = config.server.dev.port;
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+var server = app.listen(port, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+
+});
